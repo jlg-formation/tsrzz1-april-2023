9261586952b8333df4111b841a903001f9bfa001
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-04-17 11:50:28 +0200

    dev: added circles

--- front/main.js
+++ front/main.js
@@ -1,1 +1,18 @@
 console.log("start");
+
+const svgns = "http://www.w3.org/2000/svg";
+const container = document.querySelector("svg g.samples");
+
+const samples = 10;
+for (let i = 0; i < samples; i++) {
+  const circle = document.createElementNS(svgns, "circle");
+
+  const x = 34;
+  const y = 52;
+
+  const r = 1;
+  circle.setAttributeNS(null, "cx", x);
+  circle.setAttributeNS(null, "cy", y);
+  circle.setAttributeNS(null, "r", r);
+  container.appendChild(circle);
+}
