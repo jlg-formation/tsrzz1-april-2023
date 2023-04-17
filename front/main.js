84a6af4f284fbbbc05b30fb52e628a79db675ef6
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-04-17 12:06:22 +0200

    dev: added points

--- front/main.js
+++ front/main.js
@@ -1,5 +1,9 @@
 console.log("start");
 
+const cx0 = 50;
+const cy0 = 50;
+const r0 = 45;
+
 const svgns = "http://www.w3.org/2000/svg";
 const container = document.querySelector("svg g.samples");
 
@@ -7,8 +11,9 @@ const samples = 10;
 for (let i = 0; i < samples; i++) {
   const circle = document.createElementNS(svgns, "circle");
 
-  const x = 34;
-  const y = 52;
+  const angle = (i * (2 * Math.PI)) / samples;
+  const x = cx0 + r0 * Math.cos(angle);
+  const y = cy0 + r0 * Math.sin(angle);
 
   const r = 1;
   circle.setAttributeNS(null, "cx", x);
