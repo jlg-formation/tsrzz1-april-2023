a14ca3248843bdc6e488f01df7bd0b963f62d48c
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-04-17 12:15:35 +0200

    dev: added lines

--- front/main.js
+++ front/main.js
@@ -6,6 +6,7 @@ const r0 = 45;
 
 const svgns = "http://www.w3.org/2000/svg";
 const container = document.querySelector("svg g.samples");
+const lineContainer = document.querySelector("svg g.lines");
 
 const samples = 10;
 for (let i = 0; i < samples; i++) {
@@ -20,4 +21,25 @@ for (let i = 0; i < samples; i++) {
   circle.setAttributeNS(null, "cy", y);
   circle.setAttributeNS(null, "r", r);
   container.appendChild(circle);
+}
+
+const multiplicationFactor = 2;
+for (let i = 0; i < samples; i++) {
+  const line = document.createElementNS(svgns, "line");
+
+  const angle1 = (i * (2 * Math.PI)) / samples;
+  const angle2 = angle1 * multiplicationFactor;
+
+  const x1 = cx0 + r0 * Math.cos(angle1);
+  const y1 = cy0 + r0 * Math.sin(angle1);
+
+  const x2 = cx0 + r0 * Math.cos(angle2);
+  const y2 = cy0 + r0 * Math.sin(angle2);
+
+  line.setAttributeNS(null, "x1", x1);
+  line.setAttributeNS(null, "y1", y1);
+  line.setAttributeNS(null, "x2", x2);
+  line.setAttributeNS(null, "y2", y2);
+
+  lineContainer.appendChild(line);
 }
