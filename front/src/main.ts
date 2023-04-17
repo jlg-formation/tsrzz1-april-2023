6be6a3084cb47cac4c20c76436b1aa95a01f1bca
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-04-17 17:36:29 +0200

    dev: refactored getPoint

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,7 +1,7 @@
-import "./style.css";
-import { cx0, cy0, r0, samples, svgns } from "./constant";
+import { samples, svgns } from "./constant";
+import { getAngle, getPoint } from "./math";
 import { querySelector } from "./misc";
-import { getAngle } from "./math";
+import "./style.css";
 
 const container = querySelector("svg g.samples");
 const lineContainer = querySelector("svg g.lines");
@@ -10,9 +10,7 @@ for (let i = 0; i < samples; i++) {
   const circle = document.createElementNS(svgns, "circle");
 
   const angle = getAngle(i, samples);
-  const x = cx0 + r0 * Math.cos(angle);
-  const y = cy0 + r0 * Math.sin(angle);
-
+  const { x, y } = getPoint(angle);
   const r = 1;
   circle.setAttributeNS(null, "cx", x + "");
   circle.setAttributeNS(null, "cy", String(y));
@@ -27,16 +25,13 @@ for (let i = 0; i < samples; i++) {
   const angle1 = getAngle(i, samples);
   const angle2 = angle1 * multiplicationFactor;
 
-  const x1 = cx0 + r0 * Math.cos(angle1);
-  const y1 = cy0 + r0 * Math.sin(angle1);
-
-  const x2 = cx0 + r0 * Math.cos(angle2);
-  const y2 = cy0 + r0 * Math.sin(angle2);
+  const { x: x1, y: y1 } = getPoint(angle1);
+  const p2 = getPoint(angle2);
 
   line.setAttributeNS(null, "x1", x1 + "");
   line.setAttributeNS(null, "y1", y1 + "");
-  line.setAttributeNS(null, "x2", x2 + "");
-  line.setAttributeNS(null, "y2", y2 + "");
+  line.setAttributeNS(null, "x2", p2.x + "");
+  line.setAttributeNS(null, "y2", p2.y + "");
 
   lineContainer.appendChild(line);
 }
