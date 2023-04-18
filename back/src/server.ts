bf97ffed4e1a898355da4add972ad85fd95a3afa
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-04-18 15:54:24 +0200

    dev: added express code

--- back/src/server.ts
+++ back/src/server.ts
@@ -1,1 +1,14 @@
+import express from "express";
+import serveIndex from "serve-index";
+
 console.log("Starting server...");
+const publicDir = "../front/dist";
+const port = 3000;
+const app = express();
+
+app.use(express.static(publicDir));
+app.use(serveIndex(publicDir));
+
+app.listen(port, () => {
+  console.log(`Started server on port ${port}`);
+});
